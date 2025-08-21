d426156663b0e0dc3067df27ffed495734a82d1c
VictorPaesQS <viktor.paes@gmail.com>
2025-08-21 20:56:22 +0100

    Corrige index.ts para usar app.listen no Railway

--- server/index.ts
+++ server/index.ts
@@ -60,12 +60,10 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
+  const port = parseInt(process.env.PORT || "5000", 10);
+  app.listen(
     port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
-})();
+    "0.0.0.0",
+    () => log(`serving on port ${port}`)
+  );
+
